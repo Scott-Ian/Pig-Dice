0b3bfc6b1a3c7ae9495f44457cea66c7f5db7b4e
Ian Scott <chesnekov@gmail.com>
2020-06-17 13:45:41 -0700

    add UI logic to declare variables, adds roll button funcitonality, and displays rolled number to index.html
    Co-Authored-By: Nikolas Kososik <nikolas.kososik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -18,10 +18,6 @@ function Player(isTurn, tempScore, currentScore) {
   this.dice = new Dice (1, 6);
 }
 
-Player.prototype.turnCheck = function() {
-  return this.isTurn;
-}
-
 Player.prototype.tempScoreCheck = function() {
   return this.tempScore;
 }
@@ -30,13 +26,17 @@ Player.prototype.currentScoreCheck = function () {
   return this.currentScore;
 }
 
+Player.prototype.addToTempScore = function (number) {
+  this.tempScore += number;
+}
+
 Player.prototype.addToCurrentScore = function() {
   this.currentScore += this.tempScore;
   this.tempScore = 0;
 }
 
 Player.prototype.isTurnOver = function () {
-  if (this.dice.currentRole === 1) {
+  if (this.dice.currentRoll === 1) {
     return true;
   } else {
     return false;
@@ -47,17 +47,65 @@ Player.prototype.isTurnOver = function () {
 function Dice (minNumber, maxNumber) {
   this.minNumber = minNumber;
   this.maxNumber = maxNumber;
-  this.currentRole = 0;
+  this.currentRoll = 0;
 }
 
 Dice.prototype.rollDice = function() {
-  return Math.floor(Math.random() * (this.maxNumber - this.minNumber +1)) + minNumber;
+  return Math.floor(Math.random() * (this.maxNumber - this.minNumber +1)) + this.minNumber;
 }
 
 // Business Logic Functions Start Here
 
 
-
-
-
 // UI Logic
+function switchPlayers (player1, player2) {
+  let activePlayer;
+  if (player1.isTurn === true) {
+    player1.isTurn = false;
+    currentPlayer = player2;
+  } else {
+    player2.isTurn = false;
+    currentPlayer = player1;
+  }
+  return activePlayer;
+}
+
+
+$(document).ready(function () {
+  let gameStart = false;
+  let player1;
+  let player2;
+  let newGame;
+  let activePlayer;
+
+  $("button#start").click(function () {
+    player1 = new Player (true, 0, 0)
+    player2 = new Player (false, 0, 0)
+    newGame = new Game(player1, player2);
+    gameStart = true;
+    $("#play-space").show();
+  })
+
+  activePlayer = player1;
+
+  $("button#roll").click(function() {
+    if (gameStart === true) {
+      player1.dice.currentRoll = player1.dice.rollDice();
+      $("span#current-roll").text(player1.dice.currentRoll);
+      if (player1.isTurnOver() === true) {
+        alert("Turn is over");
+      } else {
+        player1.addToTempScore (player1.dice.currentRoll);   
+      }
+    } else {
+      alert("Please Start Game First");
+    }
+  });
+
+  $("button#hold").click(function () {
+
+  });
+
+
+
+});
